0d560467de6c2ce6427e00b20c4d7ccc332341cc
tony <checkmate99@naver.com>
2018-06-06 17:24:25 +0900

    basic delete
    d

--- basic/en/top/menu_aid.php
+++
@@ -1,8 +0,0 @@
-<a href="<?=KI_URL?>index.html">HOME</a>
-|
-<a href="<?=KI_URL?>customer/inquiry.html">CONTACT US</a>
-|
-<a href="<?=KI_URL?>customer/sitemap.html">SITE MAP</a>
-
-
-<? //include $inpo."top/admin/admin_main.php"?>

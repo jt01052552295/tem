4e269799f6b2cd82156620bd7c1953f2248bd821
tony <checkmate99@naver.com>
2018-09-16 12:20:09 +0900

    make file
    asdfasdf

--- ki/board/makefile.php
+++ ki/board/makefile.php
@@ -1,7 +1,5 @@
 <?php
-define('INDEX_ROOT', true); 
-//include_once('./_config.php');
-
+include_once('./_common.php');
 $menus[] = array(
 	mCode => "10",
 	title => "테스팅, 디버깅1111",
@@ -19,31 +17,23 @@ $menus[] = array(
 	title => "테스팅, 디버깅1020",
 	url => "./test/example3.html"
 );
-
-
-echo "<pre>";
 for($i=0; $i<count($menus); $i++):
 
-	
+
+	@mkdir("./test", 0777);
+    @chmod("./test", 0777);
 
 
-	if(mkdir("./test", 0777, true)){
-		chmod('./test', 0777);
-		$fh = fopen($menus[$i]['url'], "w") or die("can't open file");
-		fwrite($fh, "New Stuff 1\n"); 
-		fwrite($fh, "New Stuff 2\n"); 
-		fclose($fh);
-		print_r($menus[$i]);
-	} else {
-		die('Failed to create folders...');
-	}
-	
-
-
-
+	$f = @fopen($menus[$i]['url'], "a") or die("failed...");
+	fwrite($f, "<?php\n");
+	fwrite($f, "if (!defined('_GNUBOARD_')) exit;\n");
+	fwrite($f, "?>");
+	fclose($f);
+    @chmod($menus[$i]['url'], 0777);
 
 	
 endfor;
-echo "</pre>";
+
+
 
 ?>

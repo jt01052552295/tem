cf243e735cfe41d4cda3f9cea3f24cd3956ba8dd
jt01052552295 <jt01052552295@gmail.com>
2018-11-30 18:08:14 +0900

    pdpdpdp
    asdfasdf

--- ki/ko/pdftest.php
+++ ki/ko/pdftest.php
@@ -2,37 +2,64 @@
 define('INDEX_ROOT', true); 
 include_once('./_config.php');
 include_once('./fpdf181/rotation.php');
+include_once('./fpdi/src/Fpdi.php');
 
-class PDF extends PDF_Rotate
+class PDF extends FPDF
 {
-	function RotatedText($x,$y,$txt,$angle)
+	public $headerTxt = '';
+
+
+	// Page header
+	function Header()
 	{
-	    //Text rotated around its origin
-	    $this->Rotate($angle,$x,$y);
-	    $this->Text($x,$y,$txt);
-	    $this->Rotate(0);
+	    // Logo
+	    //$this->Image('logo.png',10,6,30);
+	    // Arial bold 15
+	    $this->SetFont('Arial','B',15);
+	    // Move to the right
+	    $this->Cell(80);
+	    // Title
+	    $this->Cell(30,10,'Title',1,0,'C');
+	    // Line break
+	    $this->Ln(20);
 	}
 
-	function RotatedImage($file,$x,$y,$w,$h,$angle)
+	// Page footer
+	function Footer()
 	{
-	    //Image rotated around its upper-left corner
-	    $this->Rotate($angle,$x,$y);
-	    $this->Image($file,$x,$y,$w,$h);
-	    $this->Rotate(0);
+	    // Position at 1.5 cm from bottom
+	    $this->SetY(-15);
+	    // Arial italic 8
+	    $this->SetFont('Arial','I',8);
+	    // Page number
+	    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
 	}
 }
 
-$pdf=new PDF();
+// Instanciation of inherited class
+
+$file = "./origin.pdf";// path: file name
+
+$pdf = new PDF();
+if (file_exists($file)){
+    $pagecount = $pdf->setSourceFile($file);
+} else {
+    die('no file');
+}
+
+
+
+$pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Arial','',20);
-$pdf->RotatedImage('./images/logo.png',10,0,40,16,0);
-//$pdf->RotatedText(100,60,'Hello!',45);
-$pdf->Cell(40,10,'Hello World!');
+$pdf->SetFont('Times','',12);
+for($i=1;$i<=40;$i++)
+    $pdf->Cell(0,10,'Printing line number '.$i,0,1);
 
 
-$dir = "./test.pdf";
 
-$pdf->Output('I', $dir); // 브라우저에서 바로 보는거
-$pdf->Output('F', $dir); // 파일 생성 ( 로컬에서 확인 서버에서 확인해보자 )
+
+
+$pdf->Output('I'); // 브라우저에서 바로 보는거
+// $pdf->Output('F', $dir); // 파일 생성 ( 로컬에서 확인 서버에서 확인해보자 )
 
 ?>

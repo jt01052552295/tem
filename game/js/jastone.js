a9bb90ea27de06e6a87b3cc7a6eb0a44999c8f96
tony <checkmate99@naver.com>
2019-04-04 22:51:32 +0900

    vv
    vv

--- game/js/jastone.js
+++ game/js/jastone.js
@@ -1,7 +1,11 @@
+var 상대덱 = document.querySelector('#rival-deck')
+var 내덱 = document.querySelector('#my-deck')
+
+var 상대영웅 = document.querySelector('#rival-hero')
+var 내영웅 = document.querySelector('#my-hero')
 
 var deckComData = [];
 var deckMyData = [];
-
 var heroComData;
 var heroMyData;
 
@@ -26,11 +30,35 @@ function cardMake(hero, myCard) {
 }
 
 
+function copyCard(data, dom, hero) { // 카드돔연결
+  var card = document.querySelector('.card-hidden .card').cloneNode(true);
+  card.querySelector('.card-cost').textContent = data.cost;
+  card.querySelector('.card-att').textContent = data.att;
+  card.querySelector('.card-hp').textContent = data.hp;
+
+  if (hero) {
+    card.querySelector('.card-cost').style.display = 'none';
+    var div = document.createElement('div');
+    div.textContent = '영웅';
+    card.appendChild(div)
+  }
+
+  card.addEventListener('click', function(){
+    console.log(card)
+  })
+
+  dom.appendChild(card)
+}
+
+
 
 function makeComputerDeck(cnt){
 	for(var i=0; i<cnt; i++){
 		deckComData.push(cardMake())
 	}
+  deckComData.forEach(function(data){
+    copyCard(data, 상대덱)
+  })
 
 }
 
@@ -38,14 +66,19 @@ function makeMyDeck(cnt){
 	for(var i=0; i<cnt; i++){
 		deckMyData.push(cardMake())
 	}
+  deckMyData.forEach(function(data){
+    copyCard(data, 내덱)
+  })
 }
 
 function createMyHero(){
-	heroComData = cardMake();
+	heroComData = cardMake(true);
+  copyCard(heroComData, 내영웅, true)
 }
 
 function createComHero(){
-	heroMyData = cardMake();
+	heroMyData = cardMake(true);
+  copyCard(heroMyData, 상대영웅, true)
 }
 
 
@@ -63,7 +96,7 @@ function init() {
 
 init(); // 진입점
 
-console.log(deckComData)
-console.log(deckMyData)
-console.log(heroComData)
-console.log(heroMyData)
+// console.log(deckComData)
+// console.log(deckMyData)
+// console.log(heroComData)
+// console.log(heroMyData)

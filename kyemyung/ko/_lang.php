aa427c3a09b1d7e94ea57edb93be023ef11726b4
jt01052552295 <jt01052552295@gmail.com>
2019-05-24 10:38:31 +0900

    key
    asdf

---
+++ kyemyung/ko/_lang.php
@@ -0,0 +1,76 @@
+<?php
+
+ $infodu['lang']['common'] = array(
+
+	txt01 => '대표 : 심 재 복 / TEL : <a href="tel:+82-55-277-2667" class="teleNum">055-277-2667</a>  I  FAX : 055-277-2687<br/>소재지 : 경남 창원시 마산회원구 자유무역3길 169',
+	txt02 => 'Copyright&copy; Kemyung CO,.LTD.  All rights reserved.',
+	txt03 => '위로',
+	txt04 => '<small>Designed by</small> 디자인을담다',
+
+
+	ft01 => 'home',	
+	ft02 => 'sitemap',
+	ft03 => 'contact us',
+	ft04 => '정보수정',
+	ft05 => '로그아웃',
+	ft06 => '관리자',
+
+
+);
+
+
+$infodu['lang']['mail'] = array(
+	txt01 => '항목은 필수 입력사항입니다.',
+	txt02 => '작성자정보',
+	txt03 => '업체명',
+	txt04 => '부서명',
+	txt05 => '담당자명',
+	txt06 => '연락처',
+	txt07 => '이메일',
+	txt08 => 'FAX',
+	txt09 => '문의내용',
+	txt10 => '문의사항',
+	txt11 => '광고필터링',
+	txt12 => '왼쪽에 보이시는 4자리 숫자를 입력해 주십시요.',
+	txt13 => '작성완료',
+	txt14 => '이름을 입력해 주십시오.',
+	txt15 => '이메일을 입력해 주세요. 작성하시는 이메일로 답변이 발송됩니다.',
+	txt16 => '광고필터링의 숫자를 똑같이 입력해 주십시오.',
+	txt17 => '처리중 입니다.',
+	txt18 => '문의신청이 실패하였습니다. 고객센터로 전화주시기 바랍니다.',
+	txt19 => '문의신청이 정상적으로 접수되었습니다.',
+	txt20 => '개인정보보호 동의 체크 해주세요.'
+);
+
+$infodu['lang']['index']['main'] = array (
+	
+	txt01 => 'pcb Total solution',
+	txt02 => 'PCB  개발부터 조립, 가공, 사출금형의 완성품까지',
+
+	pro01 => 'H/W설계, S/W개발',
+	pro02 => '마이컴프로그래밍, PC 프로그램, 전력제어,영상, R/F제어, 오디오, Video, IR, Zigbee, Bluetooth, Wifi, PC Interface, 릴레이 ON/OFF제어 web 원격제어(Internet 제어)',
+
+   	
+	card01 => 'PCB 전문기업',
+	card02 => '<strong class="mid">PCB 전문 생산 기업 (주) 계명전자 입니다.</strong><br/><br/>(주)계명전자는 0000년 설립이래 각고의 노력으로 축적해 온 기술과 경험을 바탕으로 고품질의 PCB기판을 생산 공급하고 있으며, 국내는 물론 세계의 ARTWORK 업계 발전을 위하여 제품과 기술 서비스를 제공하고 있습니다.  ',
+	card03 => 'more &gt;&gt;',
+	card04 => 'CONTACT',
+	card05 => '<strong class="tel">055 - 277 - 2667</strong><span class="fax">FAX 055-277-2687</span><span>궁금하신 사항을 문의하세요.<br/>친절히 답변 드립니다.</span>',
+
+	info01 => '사업영역',
+	info02 => '제품소개', 
+	info03 => '인증현황', 
+	info04 => '견적문의',
+	info05 => '고객센터',
+	info06 => '오시는길',
+
+
+);
+
+
+
+$infodu['lang']['company']['message'] = array (
+
+);
+
+ ?>

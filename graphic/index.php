7734efaeabdb91be4f20caac00bfb793f26641d6
jt01052552295 <jt01052552295@gmail.com>
2019-06-28 17:46:45 +0900

    fff
    ffff

---
+++ graphic/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--[if IE 7]> <html lang="ko" class="ie7"> <![endif]-->
+<!--[if IE 8]> <html lang="ko" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="ko" class="ie9"> <![endif]-->
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>html5, canvas 테스트</title>
+<meta name="apple-touch-fullscreen" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="google" content="notranslate">
+<meta name="format-detection" content="telephone=no">
+<link href="./asset/css/fonts.css?ver=1" rel="stylesheet">
+<link href="./asset/css/default.css?ver=1" rel="stylesheet">
+<link href="./asset/css/style.css?ver=1" rel="stylesheet">
+<script src="./asset/js/jquery-3.3.1.min.js?ver=1" crossorigin="anonymous"></script>
+<script src="./asset/js//jquery-2.x-git.min.js" crossorigin="anonymous"></script>
+
+<!--[if lt IE 9]>
+<script src="./asset/js/html5shiv.js"></script>
+<![endif]-->
+<!--[if lt IE 9]>
+<script src="./asset/js/respond.min.js"></script>
+<![endif]-->
+<!--[if lt IE 9]>
+<script src="./asset/js/IE9.js"></script>
+<![endif]-->
+</head>
+<body>
+
+
+부트스트랩 3.3.7 - 드롭다운메뉴 확장용, jquery ui
+
+참고해보자
+https://github.com/behigh/bootstrap_dropdowns_enhancement : MIT
+https://github.com/phstc/jquery-dateFormat : MIT
+https://github.com/jquery/jquery-mousewheel : https://github.com/jquery/jquery-mousewheel/blob/master/LICENSE.txt
+https://github.com/ccampbell/mousetrap : Apache 2.0
+https://github.com/eligrey/FileSaver.js : https://github.com/eligrey/FileSaver.js/blob/master/LICENSE.md
+https://github.com/pieroxy/lz-string/ (http://pieroxy.net/blog/pages/lz-string/index.html) :  WTFPL
+https://github.com/kig/canvasfilters : MIT
+https://www.npmjs.com/package/rgbquant : MIT
+https://github.com/jnordberg/gif.js : MIT
+
+
+</body>
+</html>

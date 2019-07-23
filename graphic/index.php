bbf4b1096657cc3b3e2176956e3f511a272d8d75
jt01052552295 <jt01052552295@gmail.com>
2019-07-23 17:58:34 +0900

    ggg
    gasdfasdf

--- graphic/index.php
+++ graphic/index.php
@@ -13,26 +13,66 @@
 <meta name="mobile-web-app-capable" content="yes">
 <meta name="google" content="notranslate">
 <meta name="format-detection" content="telephone=no">
+
 <link href="./asset/css/fonts.css?ver=1" rel="stylesheet">
 <link href="./asset/css/default.css?ver=1" rel="stylesheet">
 <link href="./asset/css/style.css?ver=1" rel="stylesheet">
+
 <script src="./asset/js/jquery-3.3.1.min.js?ver=1" crossorigin="anonymous"></script>
 <script src="./asset/js//jquery-2.x-git.min.js" crossorigin="anonymous"></script>
 
-<!--[if lt IE 9]>
-<script src="./asset/js/html5shiv.js"></script>
-<![endif]-->
-<!--[if lt IE 9]>
-<script src="./asset/js/respond.min.js"></script>
-<![endif]-->
-<!--[if lt IE 9]>
-<script src="./asset/js/IE9.js"></script>
-<![endif]-->
+<!--[if lt IE 9]><script src="./asset/js/html5shiv.js"></script><![endif]-->
+<!--[if lt IE 9]><script src="./asset/js/respond.min.js"></script><![endif]-->
+<!--[if lt IE 9]><script src="./asset/js/IE9.js"></script><![endif]-->
 </head>
 <body>
 
+<div id="">
+	<header>
+		<div class="menubar">
+			<ul>
+			 <li><a href="#">Home</a></li>
+			 <li><a href="#" id="current">Products</a>
+				<ul>
+			     <li><a href="#">Sliders</a></li>
+			     <li><a href="#">Galleries</a></li>
+			     <li><a href="#">Apps</a></li>
+			     <li><a href="#">Extensions</a></li>
+			    </ul>
+			 </li>
+			 <li><a href="#">Company</a></li>
+			 <li><a href="#">Address</a></li>
+			</ul>
+		</div>
+	</header>
 
-부트스트랩 3.3.7 - 드롭다운메뉴 확장용, jquery ui
+
+	<aside>
+		사이드
+	</aside>
+
+	<main>
+		메인
+
+	</main>
+
+
+
+</div>
+
+<!-- 컨셉 : 유틸리티 프로그램 처럼	
+1. 상단 메뉴 (드롭다운)
+2. 레프트 메뉴 (버튼 on off)
+3. 컨텐츠 부분 (캔버스) -->
+
+
+
+<!-- <canvas id='drawingCanvas' width='915' height='670'>
+  Canvas not supported
+</canvas>	 -->
+
+
+<!-- 부트스트랩 3.3.7 - 드롭다운메뉴 확장용, jquery ui
 
 참고해보자
 https://github.com/behigh/bootstrap_dropdowns_enhancement : MIT
@@ -43,7 +83,7 @@ https://github.com/eligrey/FileSaver.js : https://github.com/eligrey/FileSaver.j
 https://github.com/pieroxy/lz-string/ (http://pieroxy.net/blog/pages/lz-string/index.html) :  WTFPL
 https://github.com/kig/canvasfilters : MIT
 https://www.npmjs.com/package/rgbquant : MIT
-https://github.com/jnordberg/gif.js : MIT
+https://github.com/jnordberg/gif.js : MIT -->
 
 
 </body>

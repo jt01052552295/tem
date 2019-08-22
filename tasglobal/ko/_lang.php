e13bef8248389f362343a1e5c100c98e436d9d5d
jt01052552295 <jt01052552295@gmail.com>
2019-08-22 17:41:53 +0900

    tas
    tas

---
+++ tasglobal/ko/_lang.php
@@ -0,0 +1,133 @@
+<?php
+
+ $infodu['lang']['common'] = array(
+
+
+	txt01 => '대표이사 : 김  유  식   ㅣ    TEL : <a href="tel:+82-51-731-0056" class="teleNum">051-731-0056</a>    ㅣ   FAX : 051-416-1056<br/>소재지 : 부산광역시 강서구 낙동남로 533번길 96<br/>E-mail : tasglobal@usmtas.com',
+	txt01m => '대표이사 : 김  유  식<br/>TEL : <a href="tel:+82-51-731-0056" class="teleNum">051-731-0056</a><br/>FAX : 051-416-1056<br/>소재지 : 부산광역시 강서구 낙동남로 533번길 96<br/>E-mail : tasglobal@usmtas.com',
+	txt02 => 'Copyright&copy; TAS GLOBAL CO.,LTD.  All rights reserved.',
+	txt03 => '위로',
+	txt04 => '<small>Designed by</small> 디자인을담다',
+	txt05 => '(주)타스글로벌',
+
+
+
+
+	lang01 => 'KOREAN',
+	lang02 => 'ENGLISH',
+	lang03 => 'CHINESE',
+	lang04 => 'JAPANESE',
+
+	ft01 => '이용약관',	
+	ft02 => '개인정보처리방침',
+	ft03 => '이메일주소무단수집거부',
+	ft04 => '환자의 권리와 의무',
+	ft05 => '전화번호안내',
+	ft06 => 'CI파일 다운로드',
+	ft07 => '비급여진료비용',
+	ft08 => '사이트맵',
+
+	copy01=>'사이트선택',	
+	copy02=>'패밀리 사이트',
+	copy03=>'동아대학교',
+	copy04=>'동아대학교 의과대학',
+	copy05=>'동아대학교 평생교육원',
+
+
+);
+
+
+$infodu['lang']['mail'] = array(
+	txt01 => '항목은 필수 입력사항입니다.',
+	txt02 => '작성자정보',
+	txt03 => '업체명',
+	txt04 => '부서명',
+	txt05 => '담당자명',
+	txt06 => '연락처',
+	txt07 => '이메일',
+	txt08 => 'FAX',
+	txt09 => '문의내용',
+	txt10 => '문의사항',
+	txt11 => '광고필터링',
+	txt12 => '왼쪽에 보이시는 4자리 숫자를 입력해 주십시요.',
+	txt13 => '작성완료',
+	txt14 => '이름을 입력해 주십시오.',
+	txt15 => '이메일을 입력해 주세요. 작성하시는 이메일로 답변이 발송됩니다.',
+	txt16 => '광고필터링의 숫자를 똑같이 입력해 주십시오.',
+	txt17 => '처리중 입니다.',
+	txt18 => '문의신청이 실패하였습니다. 고객센터로 전화주시기 바랍니다.',
+	txt19 => '문의신청이 정상적으로 접수되었습니다.'
+);
+
+$infodu['lang']['index']['main'] = array (
+	
+	txt01 => '<span>T</span>echnology <span>A</span>nd <span>S</span>ervices',
+	txt02 => 'robotic hull cleaning',
+	txt03 => '당사는 자체 개발한 수중/육상 로봇 및 우수한 인력을 바탕으로<br/>전세계 어디에도 없는 경제적, 친환경적이고 안전한 선박 관리 서비스를 제공합니다.',
+
+	spoMore => 'view',
+	spo01 => '세계 해양 산업의 혁신적 파트너',
+	spo02 => '주식회사 타스글로벌은 특유의 화합, 신뢰, 개척정신을 기초로 하여 최상의 산업분석, 선박설계, 잠수 및 로보틱스를
+				융합하고, 세계 최초로 경제적이며 친환경적인 수중 로봇 청소 시스템을 완성하였습니다.<br/> 
+				주식회사 타스글로벌은 친환경적 수중 선박 청소의 기준을 마련하여 해운산업 발전에 이바지 하고자 합니다. ',
+
+	ban01 => 'Business area',
+	ban02 => '(주)타스글로벌 사업영역',
+
+	busi01 => 'Technical status',
+	busi02 => '(주)타스글로벌의 <span>기술현황</span> 입니다.',
+	busi03 => 'Certificate',
+	busi04 => '(주)타스글로벌의 <span>보유 인증서</span> 입니다.',
+	busi05 => 'E-BROCHURE',
+	busi06 => '(주)타스글로벌의 <span>브로슈어</span> 입니다.',
+	busi07 => 'Notice',
+	busi08 => '(주)타스글로벌의 <span>공지사항</span> 입니다.',
+	busi_more => 'Read More',
+
+	etc01 => '보다 나은 내일을 위한 해양산업의 친환경적 기여를 위해',
+	etc02 => '주식회사 타스글로벌은 로보틱스의 혁신을 통해 해양 산업에 공헌하고자 탄생했습니다.',
+
+	etc03 => '조선<br/>Shipbuilding',
+	etc04 => '해양<br/>Marine',
+	etc05 => '화학<br/>Chemical',
+	etc06 => '산업잠수<Br/>Commercial<br/>Diving',
+	etc07 => '로봇<br/>Robotics',
+
+
+	inq_close => '문의창 닫기',
+	inq01 => '온라인 문의',
+	inq02 => '궁금하신 사항을 문의하세요 친절히 답변 드립니다.',
+
+	inq_t01 => '고객문의',
+	inq_d01 => '051-731-0056',
+	inq_d02 => '051-416-1056',
+
+	inq_map01 => '오시는 길',
+	inq_map02 => '부산광역시 강서구 낙동남로 533번길 96',
+	
+
+	inq_m01 => '업체명',
+	inq_m02 => '담당자',
+	inq_m03 => '연락처',
+	inq_m04 => '이메일',
+	inq_m05 => '문의내용을 작성해 주세요.',
+	inq_m06 => '문의하기',
+	inq_m07 => '개인정보 취급방침에 동의하십니까?',
+	inq_m08 => '동의합니다.',
+
+	inq_err01 => '업체명을  입력해 주세요.',
+	inq_err02 => '담당자 성명을  입력해 주세요.',
+	inq_err03 => '연락처를  입력해 주세요.',
+	inq_err04 => '이메일을  입력해 주세요.',
+	inq_err05 => '문의내용을 입력해 주세요.',
+
+
+);
+
+
+
+$infodu['lang']['company']['message'] = array (
+
+);
+
+ ?>
---
+++ tasglobal/ko/_top.php
@@ -0,0 +1,320 @@
+<header id="header" class="main tran-animate" >
+
+
+
+    <div class="header_wrap " id="header_height">
+
+        <div class="headerBox">           
+            
+
+            <div class="logo"><h1><a href="<?php echo G5_LANG_URL?>"><span class="blind"><?php echo $infodu['title']?></span></a></h1></div>
+
+            
+
+            <div class="header_inner"> 
+               <?php include_once(G5_LANG_PATH.'/_menu_drop.php'); ?>
+            </div>
+
+            <div class="lang">
+                <div class="lang_m">
+                    <ul class="lang_sub" >
+                        <li><a href="#" class="active"><?php echo  $infodu['lang']['common']['lang01']?></a></li>
+                        <li><a href="#"><?php echo  $infodu['lang']['common']['lang02']?></a></li>
+                    </ul>                   
+                </div>
+            </div>
+
+            <div class="cs" id="totalMenu">
+                <div class="header_sp_first__r">
+                    <div class="header_sp_first__r__btnm">
+                        <p class="header_sp_first__r__btnm__over">
+                            <span class="line_first"></span>
+                            <span class="line_second"></span>
+                            <span class="line_thirt"></span>
+                            <span class="line_four"></span>
+                        </p>
+                    </div>
+                </div>  
+            </div>
+
+
+            <div class="subMenuBg">&nbsp;</div>
+        </div>
+
+
+        
+            
+        <!--모바일메뉴!-->
+        <div id="m_menu">
+            <a href="#"><i class="fas fa-bars fa-1x"></i></a>
+        </div>
+         <?php include_once(G5_LANG_PATH.'/_menu_drop_mobile.php'); ?>                     
+        
+
+    </div> 
+</header>
+<div id="header_overlay"></div>
+<!-- 전체메뉴 -->
+<?php include_once(G5_LANG_PATH.'/_menu_total.php'); ?>
+
+
+<?php if (!defined("_INDEX_")) { ?>
+<div class="sub_vg" style="display: block">
+    <div id="vg_con">
+        <h2 class=""><?php echo $breadcrumArr[0]['subTitle'];?></h2>
+    </div>
+</div>
+
+
+<div class="k_wrap pc_only" id="tabMiddleWrap" style="display: none">
+
+    <div class="ssnav montserrat" >
+          <ul>
+                <!-- <li style="width: 25%;"><a href="#" class=""><span>Drilling Machine</span></a></li>
+                <li style="width: 25%;"><a href="#"><span>Bandsaw</span></a></li>
+                <li style="width: 25%;"><a href="#" class="active"><span>Bevelling Machine</span></a></li>
+                <li style="width: 25%;"><a href="#"><span>Shot Blast</span></a></li> -->
+
+                <?php 
+                    $total = 0;
+                    for($j=0; $j<count($nav2nd); $j++):
+                        if(substr($nav2nd[$j]['mCode'],0,2) == $breadcrumArr[0]['mCode']):
+                            $total++;
+                        endif;
+                    endfor;
+                ?>
+
+                <?php for($j=0; $j<count($nav2nd); $j++):?>
+                    <?php if(substr($nav2nd[$j]['mCode'],0,2) == $breadcrumArr[0]['mCode']): ?>
+                        <?php
+                            
+                            if($total>0){
+                                $widthSize = 100 / $total;
+                            } else {
+                                $widthSize = 100;
+                            }
+
+                            $submenu_r = '';
+                            if($currentMenuArr['mCode'] == $nav2nd[$j]['mCode']):
+                                $submenu_r = 'active';
+                            endif;
+                        ?>
+
+
+                        <li style="width:<?php echo $widthSize?>%"><a href="<?php echo $nav2nd[$j]['url']?>" class="<?php echo $submenu_r?>"><span><?php echo $nav2nd[$j]['title']?></span></a></li>
+                    <?php endif; ?>
+                <?php endfor; ?>
+            </ul>
+    </div>
+
+
+    <div class="k_container type_center" id="tabMiddlePage">
+        <div class="vg_sub_area2">
+            <ul class="sub_tab2" >
+                <?php for($j=0; $j<count($nav2nd); $j++):?>
+                    <?php if(substr($nav2nd[$j]['mCode'],0,2) == $breadcrumArr[0]['mCode']): ?>
+                        <?php
+                            $submenu_r = '';
+                            if($currentMenuArr['mCode'] == $nav2nd[$j]['mCode']):
+                                $submenu_r = 'selected';
+                            endif;
+                        ?>
+
+
+                        <li class="tab_list "><a href="<?php echo $nav2nd[$j]['url']?>" class="<?php echo $submenu_r?>"><?php echo $nav2nd[$j]['title']?></a></li>
+                    <?php endif; ?>
+                <?php endfor; ?>
+            </ul>
+
+        </div>
+
+        <div class="vg_sub_area">
+            <ul class="sub_tab">
+                <?php 
+                    $total = count($nav2nd);
+                    if($total>0){
+                        $widthSize = 100 / $total;
+                    } else {
+                        $widthSize = 100;
+                    }
+
+                    for($j=0; $j<count($nav2nd); $j++):
+                        if(substr($nav2nd[$j]['mCode'],0,2) == $breadcrumArr[0]['mCode']):
+                            $submenu_r = '';
+                            if($currentMenuArr['mCode'] == $nav2nd[$j]['mCode']):
+                                $submenu_r = 'selected';
+                            endif;
+
+                ?>
+                            <li class="tab_list"><a href="<?php echo $nav2nd[$j]['url']?>" class="<?php echo $submenu_r?>"><?php echo $nav2nd[$j]['title']?></a></li>
+                    <?php endif; ?>
+                <?php endfor;?>
+                
+            </ul>
+        </div>
+
+
+
+    </div>
+</div>
+
+
+
+
+
+<div class="" id="subPageWrap">
+
+    <div class="subNavWrap" id="sub_navi" style="display: block">
+        <div class="subNavInner">
+            <a href="<?php echo G5_LANG_URL?>"  class="btn_home"><em>HOME</em><span class="blind">홈으로 이동</span></a>
+            <ul class="subNav1st">
+                <li class="navi_dp1">
+                    <a href="#" class="dp1">
+        
+
+                        <?php 
+                             if($breadcrumArr[0]['title']): echo $breadcrumArr[0]['title'];
+                             else: 
+                                if (function_exists('get_head_title')) {
+                                    echo get_head_title($g5['title']);
+                                }
+                             endif;
+
+                        ?>
+
+                        <span class="sub_drop bg tran-animate"><em class="blind">대분류 메뉴 펼치기</em></span>
+                    </a>
+                    <ul class="subNav2nd">
+                        <?php 
+                            for($i=0; $i<count($nav1st); $i++):
+                                $cls = "";
+                                $access = "이동";
+                                if($breadcrumArr[0]['mCode'] == $nav1st[$i]['mCode']): 
+                                    $cls = "active"; 
+                                    $access = "메뉴 활성화중";
+                                endif;
+                        
+                        ?>
+                        <li class="navi_dp2 <?php echo $cls?>">
+                            <a href="<?php echo $nav1st[$i]['url']?>" title="대분류  메뉴 <?php echo $i?>번 - <?php echo $nav1st[$i]['title']?> <?php echo $access?>"><?php echo $nav1st[$i]['title']?></a>
+                        </li>
+                        <?php endfor; ?>
+                    </ul>
+                </li>
+
+                <?php if($breadcrumArr[1]['title']):?>
+                <li class="navi_dp1">
+                    <a href="#" class="dp1"><?php echo $breadcrumArr[1]['title']?> <span class="sub_drop bg tran-animate"><em class="blind">대분류 <?php echo $breadcrumArr[1]['title']?> 하위 메뉴 펼치기</em> </span></a>
+                    
+                    <ul class="subNav2nd">
+                        <?php 
+                            for($j=0; $j<count($nav2nd); $j++):
+                                if(substr($breadcrumArr[1]['mCode'],0,2) == substr($nav2nd[$j]['mCode'],0,2)):
+                                //if(substr($currentMenuArr['mCode'],0,4) == $nav2nd[$j]['mCode']):
+                                    $cls = "";
+                                    $access = "이동";
+                                    if($breadcrumArr[1]['mCode'] == $nav2nd[$j]['mCode']): 
+                                        $cls = "active"; 
+                                        $access = "메뉴 활성화";
+                                    endif;
+                        ?>
+                                        <li class="navi_dp2 <?php echo $cls?>"><a href="<?php echo $nav2nd[$j]['url']?>" title="대분류 <?php echo $breadcrumArr[0]['title']?> 하위 메뉴 <?php echo $i?>번 - <?php echo $nav2nd[$j]['title']?> <?php echo $access?>"><?php echo $nav2nd[$j]['title']?></a></li>
+                        <?php   
+                                endif;
+                            endfor;
+                        ?>
+                    </ul>
+                    
+                    
+                </li>
+                <?php endif; ?>  
+                <?php if($breadcrumArr[2]['title']):?>
+                <li class="navi_dp1">
+                    <a href="#" class="dp1"><?php echo $breadcrumArr[2]['title']?> <span class="sub_drop bg"><em class="blind">대분류 <?php echo $breadcrumArr[2]['title']?> 하위 메뉴 펼치기</em> </span></a>
+                    
+                    <ul class="subNav2nd" data-attr="<?php echo substr($breadcrumArr[2]['mCode'],0,4)?> - <?php echo substr($nav3rd[$j]['mCode'],0,4)?>">
+                        <?php 
+                            for($j=0; $j<count($nav3rd); $j++):
+                                if(substr($breadcrumArr[2]['mCode'],0,4) == substr($nav3rd[$j]['mCode'],0,4)):
+                                //if(substr($currentMenuArr['mCode'],0,4) == $nav2nd[$j]['mCode']):
+                                    $cls = "";
+                                    $access = "이동";
+                                    if($breadcrumArr[2]['mCode'] == $nav3rd[$j]['mCode']): 
+                                        $cls = "active"; 
+                                        $access = "메뉴 활성화";
+                                    endif;
+                        ?>
+                                        <li class="navi_dp2 <?php echo $cls?>"><a href="<?php echo $nav3rd[$j]['url']?>" title="대분류 <?php echo $breadcrumArr[1]['title']?> 하위 메뉴 <?php echo $i?>번 - <?php echo $nav3rd[$j]['title']?> <?php echo $access?>"><?php echo $nav3rd[$j]['title']?></a></li>
+                        <?php   
+                                endif;
+                            endfor;
+                        ?>
+                    </ul>
+                    
+                    
+                </li>
+                <?php endif; ?>  
+            </ul>
+
+            <!-- //navi_list -->
+        </div>
+    </div>
+    <!-- //navi menu -->
+
+    <?php if (!defined("_WIDE_SCREEN_")): ?><div class="k_container type_center" id="subPage"><?php endif; ?>
+
+
+    <div class="left_wrap" style="display: none">
+        <?php include_once(G5_LANG_PATH.'/_sidebar.php'); ?>
+    </div>
+
+    <div class="contents_wrap noSidebar">
+        <div class="title_wrap">    
+            <div class="sub_vg_title">
+                <?php if($currentPrevArr['url']):?>
+                <a href="<?php echo $currentPrevArr['url']?>" class="sub_arrow sub_left_arrow"><img src="<?php echo G5_LANG_IMG_URL?>/sub_left_arrow.png" /></a>
+                <?php endif;?>
+
+
+                <h2 class="title"><?php //echo get_head_title($g5['title']); ?>
+                    <?php 
+
+                            $level = count($breadcrumArr)-1;
+                            $titleChk = true;
+                            for($b=$level; $b>=0; $b--):
+
+                                if($breadcrumArr[$b]['title']): 
+                                    echo $breadcrumArr[$b]['title']; 
+                                    $titleChk = false;
+                                    break;
+                                endif;
+                            endfor;
+
+                            if ($titleChk === true) {
+                                if (function_exists('get_head_title')) {
+                                    echo get_head_title($g5['title']);
+                                }
+                            }
+
+
+                        ?>  
+
+
+                </h2>
+
+                <?php if($currentNextArr['url']):?>
+                <a href="<?php echo $currentNextArr['url']?>" class="sub_arrow sub_right_arrow"><img src="<?php echo G5_LANG_IMG_URL?>/sub_right_arrow.png" /></a>
+                <?php endif;?>
+
+            </div>
+            <div class="breadcrum" style="display: none">
+                <?php 
+                    $breadcrum_str = '';
+                    for($b=0; $b<count($breadcrumArr); $b++):
+                        $breadcrum_str .= '&nbsp;&nbsp;&gt;&nbsp;&nbsp;'.$breadcrumArr[$b]['title'];
+                    endfor;
+                ?>
+                Home<?php echo $breadcrum_str;?>
+            </div>
+        </div>
+<?php } ?>     
